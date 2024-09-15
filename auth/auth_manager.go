d05e248dd1446ac3a396c87d67797a0b64199b52
Mohan Raj <praem1990@gmail.com>
2024-09-15 21:57:23 +0530

    Implement AuthManager

---
+++ auth/auth_manager.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"fmt"
+
+	contractsauth "github.com/goravel/framework/contracts/auth"
+	"github.com/goravel/framework/contracts/foundation"
+	"github.com/goravel/framework/contracts/http"
+)
+
+type AuthManager struct {
+	app    foundation.Application
+	ctx    http.Context
+	guards map[string]contractsauth.AuthGuardFunc
+}
+
+// GetDefaultDriver implements auth.Factory.
+func (f *AuthManager) GetDefaultDriver() contractsauth.Auth {
+	config := f.app.MakeConfig()
+	name := config.GetString("auth.defaults.guard")
+
+	return f.Guard(name)
+}
+
+// SetDefaultDriver implements auth.Factory.
+func (f *AuthManager) SetDefaultDriver(name string) contractsauth.Factory {
+	config := f.app.MakeConfig()
+	config.Add("auth.defaults.guard", name)
+
+	return f
+}
+
+// Extend implements auth.Factory.
+func (f *AuthManager) Extend(name string, callback contractsauth.AuthGuardFunc) contractsauth.Factory {
+	f.guards[name] = callback
+
+    return f
+}
+
+// Guard implements auth.Factory.
+func (f *AuthManager) Guard(name string) contractsauth.Auth {
+	driver := f.app.MakeConfig().GetString(fmt.Sprintf("auth.guards.%s.driver", name))
+
+	if guardFn, exists := f.guards[driver]; exists {
+		config := f.app.MakeConfig()
+		return guardFn(name, config, f.ctx)
+	}
+
+	return nil
+}
